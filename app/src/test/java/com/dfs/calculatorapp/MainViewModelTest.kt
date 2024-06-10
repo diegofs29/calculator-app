bfc3119d9e757e971b97fe2608add9c4b32ac292
Diego <diegofdezsrz12@gmail.com>
2024-06-10 19:37:33 +0200

    feat(calculator): create MainViewModelTest

---
+++ app/src/test/java/com/dfs/calculatorapp/MainViewModelTest.kt
@@ -0,0 +1,11 @@
+package com.dfs.calculatorapp
+
+import org.junit.Before
+
+class MainViewModelTest {
+    private lateinit var viewModel: MainViewModel
+
+    @Before fun setup() {
+        viewModel = MainViewModel()
+    }
+}
