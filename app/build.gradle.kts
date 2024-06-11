a989c44f9a193fd3f0f9e17f8c711f6309426bda
Diego <diegofdezsrz12@gmail.com>
2024-06-11 21:00:49 +0200

    feat(calculator): add Espresso dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,4 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.rules)
+    androidTestImplementation(libs.androidx.runner)
 }
